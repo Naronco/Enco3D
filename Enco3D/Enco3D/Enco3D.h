ed5bf87322f6014a9f384d7e8bf55dc46a26f309
NaroncoNexo <johannes.roth@aol.com>
2014-05-18 12:53:29 +0200

    Added typedef datatypes

--- Enco3D/Enco3D/Enco3D.h
+++ Enco3D/Enco3D/Enco3D.h
@@ -5,6 +5,23 @@
 #	undef main
 #endif
 
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long long ulonglong;
+
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed int sint32;
+typedef signed long long sint64;
+
+typedef signed long long longlong;
+
 // HEADER FILE INCLUDES
 
 // Audio Engine Headers
