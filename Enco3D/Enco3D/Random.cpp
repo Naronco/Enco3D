8dabf9cccb957b85d54c343293bccbe271d298cf
WebFreak001 <janju007@web.de>
2014-06-08 17:08:06 +0200

    Made Random a instance

--- Enco3D/Enco3D/Random.cpp
+++ Enco3D/Enco3D/Random.cpp
@@ -1,5 +1,25 @@
 #include "Random.h"
 
-unsigned long Enco3D::Core::Random::x = 235976121;
-unsigned long Enco3D::Core::Random::y = 853249712;
-unsigned long Enco3D::Core::Random::z = 345986872;
+Enco3D::Core::Random::Random()
+{
+	time_t t;
+	time(&t);
+	srand(t);
+	setSeed(rand());
+}
+
+Enco3D::Core::Random::Random(unsigned long seed)
+{
+	setSeed(seed);
+}
+
+Enco3D::Core::Random::Random(unsigned long seed1, unsigned long seed2, unsigned long seed3)
+{
+	x = seed1 + 1; // Make sure they are not zero
+	y = seed2 + 1;
+	z = seed3 + 1;
+}
+
+Enco3D::Core::Random::~Random()
+{
+}
--- Enco3D/Enco3D/Random.h
+++ Enco3D/Enco3D/Random.h
@@ -1,32 +1,54 @@
 #ifndef _ENCO3D_RANDOM_H_
 #define _ENCO3D_RANDOM_H_
 
-#include <Windows.h>
 #include <limits.h>
+#include <time.h>
+#include <random>
 
 namespace Enco3D
 {
 	namespace Core
 	{
-		/// <summary>Random class, doesn't need to be instanced</summary>
+		/// <summary>Random class, need to be instanced</summary>
 		class Random
 		{
 		private:
-			/// <summary>Random values (internal use)</summary>
-			static unsigned long x, y, z;
+			/// <summary> Random values (internal use) </summary>
+			unsigned long x = 455615615;
+			/// <summary> Random values (internal use) </summary>
+			unsigned long y = 789623456;
+			/// <summary> Random values (internal use) </summary>
+			unsigned long z = 651561538;
 
 		public:
-			/// <summary>Sets a new seed</summary>
-			/// <param name="seed">The new seed as an unsigned long</param>
-			inline static void setSeed(unsigned long seed)
+			/// <summary>Creates a new Random with random values</summary>
+			Random();
+			/// <summary>
+			/// Creates a new Random with defined seed
+			/// <param name="seed">The maximum value of the returned integer</param>
+			/// </summary>
+			Random(unsigned long seed);
+			/// <summary>Creates a new Random with all defined seeds</summary>
+			Random(unsigned long seed1, unsigned long seed2, unsigned long seed3);
+			~Random();
+
+			/// <summary>
+			/// Sets a new seed
+			/// <param name="seed">The new seed as an <see cref="unsigned long">unsigned long</see></param>
+			/// </summary>
+			inline void setSeed(unsigned long seed)
 			{
-				x = seed;
-				y = x + 34575425;
-				z = y + 97565462;
+				x = seed + 1; // Make sure they are not zero
+				y = x + 2;
+				z = y + 3;
 			}
 
-			/// <summary>Returns a random unsigned long</summary>
-			inline static unsigned long nextLong()
+			/// <summary>
+			/// Calculates a random unsigned Long
+			/// <returns>Random unsigned Long from 0 to 0xffffffffUL</returns>
+			/// <seealso cref="Random::nextInt"/>
+			/// </summary>
+			inline unsigned long nextLong()
 			{
 				x ^= x << 16;
 				x ^= x >> 5;
@@ -40,33 +62,50 @@ namespace Enco3D
 				return z;
 			}
 
-			/// <summary>Returns a random int >= 0</summary>
-			inline static int nextInt()
+			/// <summary>
+			/// Calculates a random unsigned Integer
+			/// <returns>Random unsigned Integer from 0 to 0xffffffff</returns>
+			/// <seealso cref="Random::nextInt"/>
+			/// </summary>
+			inline unsigned int nextInt()
 			{
-				return (nextLong() % LONG_MAX);
+				return (nextLong() % UINT_MAX);
 			}
 
-			/// <summary>Returns a random int from 0 to the max value</summary>
+			/// <summary>
+			/// Calculates a random Integer
 			/// <param name="max">The maximum value of the returned integer</param>
-			inline static int nextInt(int max)
+			/// <returns>Random Integer from 0 to <see cref="max">max</see></returns>
+			/// <seealso cref="Random::nextInt"/>
+			/// </summary>
+			inline unsigned int nextInt(unsigned int max)
 			{
 				return (nextInt() % max);
 			}
 
-			/// <summary>Returns a random float from 0.0f to 1.0f</summary>
-			inline static float nextFloat()
+			/// <summary>
+			/// Calculates a random Float
+			/// <returns>Random Float from 0.0 to 1.0</returns>
+			/// </summary>
+			inline float nextFloat()
 			{
-				return (nextLong() % LONG_MAX) / (float)LONG_MAX;
+				return (nextLong() % ULONG_MAX) / (long float)ULONG_MAX;
 			}
 
-			/// <summary>Returns a random double from 0.0 to 1.0</summary>
-			inline static double NextDouble()
+			/// <summary>
+			/// Calculates a random Double
+			/// <returns>Random Double from 0.0 to 1.0</returns>
+			/// </summary>
+			inline double NextDouble()
 			{
-				return (nextLong() % LONG_MAX) / (double)LONG_MAX;
+				return (nextLong() % ULONG_MAX) / (long double)ULONG_MAX;
 			}
 
-			/// <summary>Returns a random boolean (true or false)</summary>
-			inline static bool NextBool()
+			/// <summary>
+			/// Calculates a random Boolean
+			/// <returns>Random Boolean</returns>
+			/// </summary>
+			inline bool NextBool()
 			{
 				return (nextInt(2) == 0);
 			}
